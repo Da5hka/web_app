c97bf975fafbf97b01d0a7b3eb9a3dcc7481e0a5
dashzeveg.b <dashzeveg.b@corp.unitel.mn>
2023-12-08 17:18:57 +0800

    dashka web day2

--- web_app_ng/models.py
+++ web_app_ng/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class data_2023(models.Model):
-    title = models.CharField(max_length=36)
+class data_2023(models.Model): #model zarlasan
+    # NG = (
+    #     (2019, 'NG1'),
+    #     (2020, 'NG2')
+    # )
+    # Model deer baih data ymar baih gedeg heseg baina
+    title = models.CharField(max_length=255, null=True, blank=False) 
+    #jil = models.CharField(max_length=32, blank=True, null=True, choices=NG)
+    description =  models.TextField(max_length=256, blank=True, null=True)
+    picture = models.ImageField(upload_to='images/', height_field=None, width_field=None, max_length=256, blank=True, null=True)
+
+    published = models.DateField(blank=True, null=True, default=None)
+    is_published = models.BooleanField(default=False)
+
+    cover = models.FileField(upload_to='covers/', blank=True, null=True)
--- web_app_ng/views.py
+++ web_app_ng/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def first(request):
+    return HttpResponse('Hello') #end zarlagdsan baiga view-iig url-ru duudna
