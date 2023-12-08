d093f41310bc896e9565ab3be1e21e57d74c8a85
dashzeveg.b <dashzeveg.b@corp.unitel.mn>
2023-12-08 18:35:59 +0800

    dashka web day 2 last

--- web_app_ng/views.py
+++ web_app_ng/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.views import View
+from .models import data_2023
+
+class Another(View):
+
+    data_papers = data_2023.objects.all() #filter tei data nemj bolno for example: data_2023.objects.filter(is_published=True)
+    output = ''
+
+    for papers in data_papers:
+
+        output += f"We have {papers.title} papers with attach {papers.cover} {papers.published}. <br>"
+
+    def get(self, request):
+        return HttpResponse(self.output)
+
 def first(request):
     return HttpResponse('Hello') #end zarlagdsan baiga view-iig url-ru duudna
